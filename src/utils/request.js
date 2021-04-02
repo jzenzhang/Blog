63cd6cd9b92e9c1d319b74d07c861ef0668f59f5
zhangjinzhe <izhangjinzhe@outlook.com>
2021-04-02 11:49:35 +0800

    :init:

--- src/utils/request.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const fetch = axios.create({
-  baseURL: '',
-  timeout: 10000
-})
-
-export default fetch
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  proxy: {
-    '/app': {
-      target: 'http://127.0.0.1:8080',
-      changeOrigin: true,
-      rewrite: path => path.replace(/^\//, '')
-    }
-  }
-}
